b8eef835c2aa4a45f0c7ce0250782ceb6c70a85b
bramuno <bramuno@users.noreply.github.com>
2023-03-12 10:39:11 -0700

    Update ESP32.ino

--- ESP32.ino
+++ ESP32.ino
@@ -9,14 +9,15 @@ DallasTemperature sensor0(&oneWire0);
 int iteration=0;int i=0;
 int last = millis();
 
-////////////
+////////////  edit these values for each sensor 
 const char* ssid     = "SSIDnameHere";     // your network SSID (name of wifi network)
 const char* password = "123123123"; // your network password
 const char * udpAddress = "192.168.1.100";  // your syslog server destination IP
-const int udpPort = 3333;  // leave this unless you know what you're doing
-boolean connected = false; //start in disconnected state
-WiFiUDP udp; // leave this unless you know what you're doing
-///////////
+/////////// no edits below this line unless you know what you are doing
+const int udpPort = 3333;  
+boolean connected = false; 
+WiFiUDP udp; 
+
 void setup() {
  //Initialize serial and wait for port to open:
  Serial.begin(115200);
@@ -45,27 +46,31 @@ void(* resetFunc) (void) = 0; //declare reset function at address 0 - MUST BE AB
 
 void loop() {
   iteration++;  Serial.print("iteration = ");  Serial.println(iteration);
-  //if (iteration > 1000)resetFunc(); //or just reset after 1000 loops
+  if (iteration > 100 )resetFunc(); //or just reset after 1000 loops
   
 sensor0.requestTemperatures();  // send command 
 float temp0C = sensor0.getTempCByIndex(0);
 float temp0F = (temp0C*9/5) + 32;
-
  Serial.print("Temperature #0 is: "); 
  Serial.print(temp0C);
  Serial.print(" C / "); 
  Serial.print(temp0F);
  Serial.println(" F"); 
+ mydata = String(temp0F) + " " + location;
+ Serial.print("sending packet: '"); 
+ Serial.print(mydata); 
+ Serial.println("'"); 
+
 
 // send to listening server
 if(connected){
     //Send a packet
     udp.beginPacket(udpAddress,udpPort);
-    udp.print(temp0F);
+    udp.print(mydata);
     udp.endPacket();
   }
 else{
- resetFunc();  // RESET IT DISCONNECTED
+ resetFunc();  // RESET IF DISCONNECTED
 }
 
  delay(5000); 
